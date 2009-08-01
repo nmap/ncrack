592fa4caaee410563a4adda6bb520e6cf1ad0474
ithilgore <ithilgore@e0a8ed71-7df4-0310-8962-fdc924857419>
2009-08-01 00:48:33 +0000

    Some more fixes to get Windows to work with the latest changes. setup-exe updated.

--- ncrack_winconfig.h
+++ ncrack_winconfig.h
@@ -3,8 +3,11 @@
 /* Without this, Windows will give us all sorts of crap about using functions
    like strcpy() even if they are done safely */
 #define _CRT_SECURE_NO_DEPRECATE 1
-#define NCRACK_PLATFORM "i686-pc-windows-windows"
 
+#define NCRACK_NAME "Ncrack"
+#define NCRACK_URL "http://ncrack.org"
+#define NCRACK_PLATFORM "i686-pc-windows-windows"
+#define NCRACKDATADIR "C:\\Program Files\\Ncrack" /* FIXME: I really need to make this dynamic */
 
 #define HAVE_OPENSSL 1
 /* Apparently __func__ isn't yet supported */
