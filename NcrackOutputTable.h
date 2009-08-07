1e4d960574ed43097a6452a1befbb5be0296f926
ithilgore <ithilgore@e0a8ed71-7df4-0310-8962-fdc924857419>
2009-08-07 21:48:22 +0000

    Fixed license headers for NcrackOutputTable files.

--- NcrackOutputTable.h
+++ NcrackOutputTable.h
@@ -1,20 +1,20 @@
 
 /***************************************************************************
- * NcrackOutputTable.h -- A relatively simple class for organizing Ncrack      *
+ * NcrackOutputTable.h -- A relatively simple class for organizing Ncrack  *
  * output into an orderly table for display to the user.                   *
  *                                                                         *
- ***********************IMPORTANT Ncrack LICENSE TERMS************************
+ ***********************IMPORTANT NMAP LICENSE TERMS************************
  *                                                                         *
- * The Ncrack Security Scanner is (C) 1996-2009 Insecure.Com LLC. Ncrack is    *
+ * The Nmap Security Scanner is (C) 1996-2009 Insecure.Com LLC. Nmap is    *
  * also a registered trademark of Insecure.Com LLC.  This program is free  *
  * software; you may redistribute and/or modify it under the terms of the  *
  * GNU General Public License as published by the Free Software            *
  * Foundation; Version 2 with the clarifications and exceptions described  *
  * below.  This guarantees your right to use, modify, and redistribute     *
- * this software under certain conditions.  If you wish to embed Ncrack      *
+ * this software under certain conditions.  If you wish to embed Nmap      *
  * technology into proprietary software, we sell alternative licenses      *
  * (contact sales@insecure.com).  Dozens of software vendors already       *
- * license Ncrack technology such as host discovery, port scanning, OS       *
+ * license Nmap technology such as host discovery, port scanning, OS       *
  * detection, and version detection.                                       *
  *                                                                         *
  * Note that the GPL places important restrictions on "derived works", yet *
@@ -22,29 +22,29 @@
  * misunderstandings, we consider an application to constitute a           *
  * "derivative work" for the purpose of this license if it does any of the *
  * following:                                                              *
- * o Integrates source code from Ncrack                                      *
- * o Reads or includes Ncrack copyrighted data files, such as                *
- *   Ncrack-os-db or Ncrack-service-probes.                                    *
- * o Executes Ncrack and parses the results (as opposed to typical shell or  *
- *   execution-menu apps, which simply display raw Ncrack output and so are  *
+ * o Integrates source code from Nmap                                      *
+ * o Reads or includes Nmap copyrighted data files, such as                *
+ *   nmap-os-db or nmap-service-probes.                                    *
+ * o Executes Nmap and parses the results (as opposed to typical shell or  *
+ *   execution-menu apps, which simply display raw Nmap output and so are  *
  *   not derivative works.)                                                * 
- * o Integrates/includes/aggregates Ncrack into a proprietary executable     *
+ * o Integrates/includes/aggregates Nmap into a proprietary executable     *
  *   installer, such as those produced by InstallShield.                   *
  * o Links to a library or executes a program that does any of the above   *
  *                                                                         *
- * The term "Ncrack" should be taken to also include any portions or derived *
- * works of Ncrack.  This list is not exclusive, but is meant to clarify our *
+ * The term "Nmap" should be taken to also include any portions or derived *
+ * works of Nmap.  This list is not exclusive, but is meant to clarify our *
  * interpretation of derived works with some common examples.  Our         *
- * interpretation applies only to Ncrack--we don't speak for other people's  *
+ * interpretation applies only to Nmap--we don't speak for other people's  *
  * GPL works.                                                              *
  *                                                                         *
  * If you have any questions about the GPL licensing restrictions on using *
- * Ncrack in non-GPL works, we would be happy to help.  As mentioned above,  *
- * we also offer alternative license to integrate Ncrack into proprietary    *
+ * Nmap in non-GPL works, we would be happy to help.  As mentioned above,  *
+ * we also offer alternative license to integrate Nmap into proprietary    *
  * applications and appliances.  These contracts have been sold to dozens  *
  * of software vendors, and generally include a perpetual license as well  *
  * as providing for priority support and updates as well as helping to     *
- * fund the continued development of Ncrack technology.  Please email        *
+ * fund the continued development of Nmap technology.  Please email        *
  * sales@insecure.com for further information.                             *
  *                                                                         *
  * As a special exception to the GPL terms, Insecure.Com LLC grants        *
@@ -65,13 +65,13 @@
  * This also allows you to audit the software for security holes (none     *
  * have been found so far).                                                *
  *                                                                         *
- * Source code also allows you to port Ncrack to new platforms, fix bugs,    *
+ * Source code also allows you to port Nmap to new platforms, fix bugs,    *
  * and add new features.  You are highly encouraged to send your changes   *
- * to Ncrack-dev@insecure.org for possible incorporation into the main       *
+ * to nmap-dev@insecure.org for possible incorporation into the main       *
  * distribution.  By sending these changes to Fyodor or one of the         *
  * Insecure.Org development mailing lists, it is assumed that you are      *
- * offering the Ncrack Project (Insecure.Com LLC) the unlimited,             *
- * non-exclusive right to reuse, modify, and relicense the code.  Ncrack     *
+ * offering the Nmap Project (Insecure.Com LLC) the unlimited,             *
+ * non-exclusive right to reuse, modify, and relicense the code.  Nmap     *
  * will always be available Open Source, but this is important because the *
  * inability to relicense code has caused devastating problems for other   *
  * Free Software projects (such as KDE and NASM).  We also occasionally    *
@@ -84,7 +84,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       *
  * General Public License v2.0 for more details at                         *
  * http://www.gnu.org/licenses/gpl-2.0.html , or in the COPYING file       *
- * included with Ncrack.                                                     *
+ * included with Nmap.                                                     *
  *                                                                         *
  ***************************************************************************/
 
