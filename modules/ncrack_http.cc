8e709589f980fdeeac7c89cbd6355977145f95e6
ithilgore <ithilgore@e0a8ed71-7df4-0310-8962-fdc924857419>
2009-08-08 01:50:05 +0000

    Fixed syntax error on this one.

--- modules/ncrack_http.cc
+++ modules/ncrack_http.cc
@@ -461,9 +461,9 @@ http_basic(nsock_pool nsp, Connection *con)
        * then it means our credentials were correct.
        */
       if (memsearch((const char *)con->iobuf->get_dataptr(),
-            "200 OK", con->iobuf->get_len())) {
+            "200 OK", con->iobuf->get_len()) 
           || memsearch((const char *)con->iobuf->get_dataptr(),
-            "301", con->iobuf->get_len()) {
+            "301", con->iobuf->get_len())) {
         con->auth_success = true;
       }
       delete con->iobuf;
