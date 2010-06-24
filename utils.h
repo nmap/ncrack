e6589be858c5c537f611a427d4328a4fbc8857c0
ithilgore <ithilgore@e0a8ed71-7df4-0310-8962-fdc924857419>
2010-06-24 09:23:08 +0000

    Removed optcmp from utils.cc since nbase has it now.

--- utils.h
+++ utils.h
@@ -160,12 +160,6 @@ char *chomp(char *string);
 char *memsearch(const char *haystack, const char *pneedle, size_t bytes);
 
 
-/* Compare a canonical option name (e.g. "max-scan-delay") with a
-   user-generated option such as "max_scan_delay" and returns 0 if the
-   two values are considered equivalant (for example, - and _ are
-   considered to be the same), nonzero otherwise. */
-int optcmp(const char *a, const char *b);
-
 /* convert string to protocol number */
 u8 str2proto(char *str);
 
