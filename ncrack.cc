535502da351853e4c20feebc399d6196e7806deb
ithilgore <ithilgore@e0a8ed71-7df4-0310-8962-fdc924857419>
2010-08-23 19:06:40 +0000

    Call ncrack_rdp only when system has OpenSSL

--- ncrack.cc
+++ ncrack.cc
@@ -720,9 +720,9 @@ call_module(nsock_pool nsp, Connection *con)
     ncrack_pop3(nsp, con);
   else if (!strcmp(name, "pop3s"))
     ncrack_pop3(nsp, con);
+#if HAVE_OPENSSL
   else if (!strcmp(name, "rdp")) 
     ncrack_rdp(nsp, con);
-#if HAVE_OPENSSL
   else if (!strcmp(name, "smb"))
     ncrack_smb(nsp, con);
 #endif
