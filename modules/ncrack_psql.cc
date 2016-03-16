22150faddb2e5a2c3cd2868390d6c54b435763e8
ithilgore <fotis.hantzis@gmail.com>
2016-03-15 22:35:58 -0500

    latest psql module by edeirme

--- modules/ncrack_psql.cc
+++ modules/ncrack_psql.cc
@@ -87,7 +87,6 @@
  *                                                                         *
  ***************************************************************************/
 
-
 #include "ncrack.h"
 #include "nsock.h"
 #include "NcrackOps.h"
@@ -101,8 +100,10 @@
 #define PSQL_AUTH_TYPE 4
 #define PSQL_SALT 4
 
-extern void ncrack_read_handler(nsock_pool nsp, nsock_event nse, void *mydata);
-extern void ncrack_write_handler(nsock_pool nsp, nsock_event nse, void *mydata);
+extern void ncrack_read_handler(nsock_pool nsp, nsock_event nse, void
+*mydata);
+extern void ncrack_write_handler(nsock_pool nsp, nsock_event nse, void
+*mydata);
 extern void ncrack_module_end(nsock_pool nsp, void *mydata);
 static int psql_loop_read(nsock_pool nsp, Connection *con);
 unsigned char charToHexDigit(char c);
@@ -110,7 +111,8 @@ enum states { PSQL_INIT, PSQL_AUTH, PSQL_FINI };
 
 
 
-/* n is the size of src. dest must have at least n * 2 + 1 allocated bytes. */
+/* n is the size of src. dest must have at least n * 2 + 1 allocated bytes.
+*/
 static char *enhex(char *dest, const unsigned char *src, size_t n)
 {
     unsigned int i;
@@ -122,160 +124,124 @@ static char *enhex(char *dest, const unsigned char *src, size_t n)
 }
 
 
-/* Arguments are assumed to be non-NULL, with the exception of nc and cnonce,
+/* Arguments are assumed to be non-NULL, with the exception of nc and
+cnonce,
    which may be garbage only if qop == QOP_NONE. */
 static void make_response(char buf[MD5_DIGEST_LENGTH * 2 + 3 + 1],
     const char *username, const char *password, const char *salt)
 {
-    char HA1_hex[MD5_DIGEST_LENGTH * 2 + 1], HA2_hex[MD5_DIGEST_LENGTH * 2 + 1];
+    char HA1_hex[MD5_DIGEST_LENGTH * 2 + 1], HA2_hex[MD5_DIGEST_LENGTH * 2
++ 1];
     unsigned char hashbuf[MD5_DIGEST_LENGTH];
     char finalhash[MD5_DIGEST_LENGTH * 2 + 3];
     MD5_CTX md5;
-    salt = "6e519fe2";
-    printf("New username: %s\n",username);
-    
+
     /* Calculate MD5(Password + Username) */
     MD5_Init(&md5);
-
     MD5_Update(&md5, password, strlen(password));
     MD5_Update(&md5, username, strlen(username));
     MD5_Final(hashbuf, &md5);
     enhex(HA1_hex, hashbuf, sizeof(hashbuf));
-    //printf("First hash: %s", HA1_hex);
+
     /* Calculate response MD5(above + Salt). */
     MD5_Init(&md5);
     MD5_Update(&md5, HA1_hex, strlen(HA1_hex));
     MD5_Update(&md5, salt, strlen(salt));
     MD5_Final(hashbuf, &md5);
     enhex(buf, hashbuf, sizeof(hashbuf));
-    //printf("Hash: %s \n",hashbuf);
-    char md55[4] = "md5";
-    //memcpy(md55,finalhash, sizeof(md55));
-    //memcpy(hashbuf, finalhash, sizeof(buf));
-    strcpy(finalhash,"md5");
-    strcat(finalhash, buf);
-    // printf("Yolo: %s \n", finalhash);
-    // printf("asdad %d \n", strlen(finalhash));
-    // printf("asdad %d \n", strlen(buf));
+
+    /* Add the string md5 at the beggining. */
+    strncpy(finalhash,"md5", sizeof("md5"));
+    strncat(finalhash, buf, sizeof(finalhash));
     buf[0] = '\0';
-    // printf("hehe : %d\n", MD5_DIGEST_LENGTH * 2 + 3);
     strncpy(buf, finalhash, MD5_DIGEST_LENGTH * 2 + 3);
     buf[MD5_DIGEST_LENGTH * 2 + 3] = '\0';
-    // printf("asdad %d \n", strlen(buf));
 }
 
 static int
-psql_loop_read(nsock_pool nsp, Connection *con, char *psql_code_ret, char *psql_salt_ret)
+psql_loop_read(nsock_pool nsp, Connection *con, char *psql_code_ret, char
+*psql_salt_ret)
 {
   int i,j = 0;
   char psql_code[PSQL_DIGITS + 1]; /* 1 char + '\0' */
-  char psql_type[2];
-  char psql_packet_length[PSQL_PACKET_LENGTH + 1]; /* 4 chars + '\0' */
+  char psql_salt[PSQL_SALT + 1]; /* 4 + '\0' */
   char dig[2]; /* temporary digit string */
-  u_char *p2;
   char *p;
   int packet_length;
-  int num;  
+  int authentication_type;
+
   if (con->inbuf == NULL || con->inbuf->get_len() < PSQL_DIGITS + 1) {
     nsock_read(nsp, con->niod, ncrack_read_handler, PSQL_TIMEOUT, con);
     return -1;
   }
-  /* Make sure first 5 (PSQL_DIGITS) bytes of each line are something */
+
+  /* Get the first character */
   p = (char *)con->inbuf->get_dataptr();
   dig[1] = '\0';
   for (i = 0; i < PSQL_DIGITS; i++) {
-    dig[0] = *p++;
-    psql_code[i] = dig[0];
+    psql_code[i] = *p++;
   }
   psql_code[1] = '\0';
   strncpy(psql_code_ret, psql_code, PSQL_DIGITS);
 
   if (!strncmp(psql_code_ret, "R", PSQL_DIGITS)) {
     /* Read packet length only if it is of type R */
-    char array[3];
+
+    /* Currently we care only for the last byte.
+       The packet length will always be small enough
+       to fit in one byte */
     for (i = 0; i < PSQL_PACKET_LENGTH; i++) {
-     dig[0] = *p++;
-     sprintf(array, "\n%x", dig[0]);
-     num = (int)strtol(array, NULL, 16);
+     snprintf(dig, 3, "\n%x", *p++);
+     packet_length = (int)strtol(dig, NULL, 16);
     }
-
-      /* Currently we care only for the last byte.
-       The salt packet will always be small enough 
-       to fit in that byte */
-
-    if (con->inbuf->get_len() < num + 1) {
+    if (con->inbuf->get_len() < packet_length + 1) {
       nsock_read(nsp, con->niod, ncrack_read_handler, PSQL_TIMEOUT, con);
       return -1;
     }
-    
-    /* At this point we will need to know the authentication type. 
-      Possible values are 5 and 0. 5 stands for MD5 and 0 stands for successful
-      authentication. If it is 5 we are at the second stage of the process PSQL_AUTH
-      while if it is 0 we are at the third stage PSQL_FINI.
-      This value consists of 4 bytes but only the fourth will have a value
-      e.g. 00 00 00 05 for MD5 or 00 00 00 00 for successful authentication. */
 
-    char array2[5];
+    /* At this point we will need to know the authentication type.
+      Possible values are 5 and 0. 5 stands for MD5 and 0 stands for
+      successful authentication. If it is 5 we are at the second stage of
+      the process PSQL_AUTH while if it is 0 we are at the third stage PSQL_FINI.
+      This value consists of 4 bytes but only the fourth will have a value
+      e.g. 00 00 00 05 for MD5 or 00 00 00 00 for successful authentication.
+     */
     for (i = 0; i < PSQL_AUTH_TYPE; i++) {
-      dig[0] = *p++;
-      sprintf(array2, "\n%x", dig[0]);
-      num = (int)strtol(array2, NULL, 16);
+      snprintf(dig, 3, "\n%x", *p++);
+      authentication_type = (int)strtol(dig, NULL, 16);
     }
 
+
     /* If authentication type is 'MD5 password' (carries Salt) read salt */
-    if (num == 5) {
-      printf("Authentication type: MD5");
-      p2 = (u_char *)con->inbuf->get_dataptr();
-      char psql_salt[PSQL_SALT*2 + 1]; /* 8 characters + null terminator*/
-      char psql_salt_good[3];
-      psql_type[1] = '\0';
+    if (authentication_type == 5) {
 
-      for(i = 0; i < con->inbuf->get_len() - 1; i++){
-       *p2++;  
-
-       /* The following proccess will omit leading 0.
-        In that case we will manually add one. */
-
-       if (i == con->inbuf->get_len() - 5){
-         sprintf(psql_salt_good, "%x", *p2);
-         if (strlen(psql_salt_good) < 2)
-           sprintf(psql_salt_good, "0%x", *p2);
-         strcpy(psql_salt,psql_salt_good);
-         j++;
-       }
-       else if (i >= con->inbuf->get_len() - 5){
-         sprintf(psql_salt_good, "%x", *p2);
-         if (strlen(psql_salt_good)< 2)
-           sprintf(psql_salt_good, "0%x", *p2);
-         strcat(psql_salt, psql_salt_good);
-         j++;
-       } 
-       
+      for (i = 0; i < 4; i++){
+        psql_salt[i] = *p++;
       }
-      psql_salt[PSQL_SALT * 2] = '\0';
+      psql_salt[4] = '\0';
+      strncpy(psql_salt_ret, psql_salt, PSQL_SALT);
 
-      printf("\nSalt: %s\n", psql_salt);
-    
-      strncpy(psql_salt_ret, psql_salt, PSQL_SALT*2);
       return 0;
 
     }
-    else if (num == 0) {
+    else if (authentication_type == 0)
+      /* Successful authentication */
       return 1;
-    }
 
   } else if (!strncmp(psql_code_ret, "E", PSQL_DIGITS)) {
 
     /* Error packet. The login attempt has failed.
       Perhaps we could do some more validation on the packet.
       Currently any kind of packet with E as the first byte will
-      be interpreted as a Error package. It is only a matter 
+      be interpreted as a Error package. It is only a matter
       of concerns if the service is not a Postgres service.  */
+
     return 0;
 
   }
-    /* Malformed packet. Exit safely. */ 
-    return -2;  
+
+    /* Malformed packet. Exit safely. */
+    return -2;
 }
 
 void
@@ -287,16 +253,15 @@ ncrack_psql(nsock_pool nsp, Connection *con)
 
   char packet_length;
   char psql_code[PSQL_DIGITS + 1];
-  char psql_salt[PSQL_SALT*2 + 1];
-  
+  char psql_salt[PSQL_SALT + 1];
   memset(psql_code, 0, sizeof(psql_code));
   memset(psql_salt, 0, sizeof(psql_salt));
-   
+
   char response_hex[MD5_DIGEST_LENGTH *2 + 3];
   switch (con->state)
   {
     case PSQL_INIT:
-     
+
       con->state = PSQL_AUTH;
       delete con->inbuf;
       con->inbuf = NULL;
@@ -308,12 +273,9 @@ ncrack_psql(nsock_pool nsp, Connection *con)
       packet_length = strlen(con->user) + 7 +
           strlen("\x03user  database postgres application_name psql client_encoding UTF8  ");
 
-      printf("Username: %s\n", con->user);
-      printf("Password: %s\n", con->pass);
-
-      con->outbuf->snprintf(packet_length,
-           "%c%c%c%c%c\x03%c%cuser%c%s%cdatabase%cpostgres%capplication_name%cpsql%cclient_encoding%cUTF8%c%c", 
-           0,0,0,packet_length,0,0,0,0,con->user,0,0,0,0,0,0,0,0);
+      con->outbuf->snprintf(packet_length, 
+          "%c%c%c%c%c\x03%c%cuser%c%s%cdatabase%cpostgres%capplication_name%cpsql%cclient_encoding%cUTF8%c%c",
+          0,0,0,packet_length,0,0,0,0,con->user,0,0,0,0,0,0,0,0);
       nsock_write(nsp, nsi, ncrack_write_handler, PSQL_TIMEOUT, con,
         (const char *)con->outbuf->get_dataptr(), con->outbuf->get_len());
 
@@ -324,12 +286,13 @@ ncrack_psql(nsock_pool nsp, Connection *con)
       if (psql_loop_read(nsp, con, psql_code, psql_salt) < 0)
         break;
 
-      
-      make_response(response_hex, con->user + strlen("md5") , con->pass, psql_salt);
-      printf("%s\n",response_hex);
+      if (!strncmp(psql_code, "E", PSQL_DIGITS))
+        return ncrack_module_end(nsp, con);
+
+      make_response(response_hex, con->user , con->pass, psql_salt);
 
       response_hex[MD5_DIGEST_LENGTH * 2 + 3] = '\0';
-      
+
       con->state = PSQL_FINI;
       delete con->inbuf;
       con->inbuf = NULL;
@@ -341,11 +304,10 @@ ncrack_psql(nsock_pool nsp, Connection *con)
       con->outbuf = new Buf();
       packet_length = strlen(response_hex) + 5 + strlen("p");
 
-      /* This packet will not count the last null byte in packet length byte,
-        that is why we remove one from snprintf. */
-      con->outbuf->snprintf(packet_length,
-            "p%c%c%c%c%s%c",0,0,0,packet_length - 1,response_hex,0
-        );
+      /* This packet will not count the last null byte in packet length
+        byte, that is why we remove one from snprintf. */
+      con->outbuf->snprintf(packet_length, 
+          "p%c%c%c%c%s%c",0,0,0,packet_length - 1,response_hex,0);
 
       nsock_write(nsp, nsi, ncrack_write_handler, PSQL_TIMEOUT, con,
         (const char *)con->outbuf->get_dataptr(), con->outbuf->get_len());
@@ -356,7 +318,7 @@ ncrack_psql(nsock_pool nsp, Connection *con)
       if (psql_loop_read(nsp, con, psql_code, psql_salt) < 0)
         break;
       else if (psql_loop_read(nsp, con , psql_code, psql_salt) == 1)
-        con->auth_success = true; 
+        con->auth_success = true;
 
       con->state = PSQL_INIT;
 
